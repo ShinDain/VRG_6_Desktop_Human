9a9417627b10e1b539c46e107c9cf8913d821c44
ShinDain <34495918+ShinDain@users.noreply.github.com>
2023-11-13 14:44:48 +0900

    python plugIn / etc...
    python plugIn 테스트
    gamemode 파일 경로 버그 수정

--- Source/VRG_D_Human/VRG_D_HumanGameMode.cpp
+++ Source/VRG_D_Human/VRG_D_HumanGameMode.cpp
@@ -7,9 +7,9 @@
 AVRG_D_HumanGameMode::AVRG_D_HumanGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	/*static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
-	}
+	}*/
 }
