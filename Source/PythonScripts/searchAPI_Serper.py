3704a8ceb9a40af3cd5b46776f997847e0b146c0
Park Ki Jeoung <parkyj477@gmail.com>
2023-12-09 06:30:26 +0900

    SerperAPI를 이용한 인터넷 검색 제공
    chat_respons( text ) 함수를 제공. 답변을 리턴함.
    SerperAPI를 이용하여 가장 답변의 가치가 높다. 단, 한달 검색이 2500회로 제한되어있다.
    한번의 질문이 한번의 검색만 사용하는 것은 아니기에 약 1200회 가량의 답변이 가능하다고 예상된다.

---
+++ Source/PythonScripts/searchAPI_Serper.py
@@ -0,0 +1,39 @@
+import os
+import configparser
+
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+
+api_config = configparser.ConfigParser()
+api_config.read_file(open('apidata.ini'))
+os.environ['OPENAI_API_KEY'] = api_config["OPENAI"]["OTHER_KEY"]
+os.environ['GOOGLE_CSE_ID'] = api_config["GOOGLE"]["CSE_ID"]
+os.environ['GOOGLE_API_KEY'] = api_config["GOOGLE"]["API_KEY"]
+os.environ['SERPER_API_KEY']= api_config["OPENAI"]["SERPER_KEY"]
+
+ask_prompt = PromptTemplate(
+    input_variables=["contents"],
+    template="다음 질문을 한국어로 답해줘. '{contents}'"
+)
+
+llm = OpenAI(temperature=0)
+
+serper_chain = load_tools(["google-serper", "llm-math"], llm=llm)
+serper_decider = initialize_agent(serper_chain,
+                                  llm,
+                                  agent="zero-shot-react-description",
+                                  verbose=True)
+
+def chat_respons(input_text):
+    result = serper_decider.run(ask_prompt.format(contents=input_text))
+    return result
+
+
+if __name__ == '__main__':
+    print("GoogleSerperAPI를 이용한 인터넷 검색 AI 입니다.")
+    qurey = input("질문을 입력해주세요 : ")
+    result = chat_respons(qurey)
+    print(result)
+    input("Press Enter to end Program")
