375b75ede3102e1934e38a0f18ef1cfb15fe7136
Park Ki Jeoung <parkyj477@gmail.com>
2023-12-11 14:51:03 +0900

    URL 획득 시 마우스 이동하지 않도록 수정

--- Source/PythonScripts/GetURLInChrome_pyautogui.py
+++ Source/PythonScripts/GetURLInChrome_pyautogui.py
@@ -9,19 +9,9 @@ def get_active_window_url():
 
     # 활성 창이 크롬 브라우저인지 확인
     if "Chrome" in active_window.title:
-        # 현재 마우스 위치를 기록
-        origin_location = pyautogui.position()
-        # 크롬 브라우저에서 주소창의 위치
-        address_bar_location = (160, 60)
-
-        # 마우스를 주소창으로 이동시키고 URL 선택
-        pyautogui.click(active_window.left + address_bar_location[0], active_window.top + address_bar_location[1])
-        pyautogui.hotkey('ctrl', 'a')  # URL 전체 선택
+        pyautogui.hotkey('ctrl', 'l')  # URL 전체 선택
         pyautogui.hotkey('ctrl', 'c')  # 클립보드로 복사
-        pyautogui.click(button='left')
-
-        # 마우스를 원래 위치로 이동
-        pyautogui.moveTo(origin_location)
+        pyautogui.hotkey('esc')  # 드래그 취소
 
         # 클립보드에서 URL 가져오기
         url = pyperclip.paste()
