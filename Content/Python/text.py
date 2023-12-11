f8e526de372259af6fe329d704ad43951826bc49
kim-minhan <alsgks7460@naver.com>
2023-12-11 14:53:06 +0900

    text 수정

--- Content/Python/text.py
+++ Content/Python/text.py
@@ -9,8 +9,21 @@ def get_text_from_url(url):
         # BeautifulSoup을 사용하여 HTML 파싱
         soup = BeautifulSoup(response.text, 'html.parser')
 
-        # 링크에 "news.naver.com"이 포함되어 있는지 확인
-        if "news.naver.com" in url:
+        # 링크에 "sports.news.naver.com"이 포함되어 있는지 확인
+        if "sports.news.naver.com" in url:
+            # sports.news.naver.com인 경우 다른 방식으로 내용 추출
+            # 예: 기사 내용을 포함하는 <div> 태그 추출
+            article_tag = soup.find('div', class_='content')
+
+            # <div> 태그가 존재하면 해당 내용을 추출, 없으면 빈 문자열 반환
+            if article_tag:
+                sports_content = article_tag.get_text(separator=' ', strip=True)
+                return sports_content
+            else:
+                print("스포츠 기사 내용을 찾을 수 없습니다.")
+                return ""
+
+        elif "news.naver.com" in url:
             # news.naver.com인 경우 다른 방식으로 내용 추출
             # 기사 내용을 포함하는 <article> 태그 추출
             article_tag = soup.find('article', class_='go_trans _article_content')
@@ -27,7 +40,7 @@ def get_text_from_url(url):
             body_content = soup.find('body').get_text(separator=' ', strip=True)
             return body_content
     else:
-        print(f"Error: {response.status_code}")
+        print(f"에러: {response.status_code}")
         return None
 
 
@@ -41,3 +54,5 @@ if __name__ == "__main__":
     # 결과 출력 (띄어 쓰기 없애기)
     if text_content:
         print(text_content)
+
+
